13b7de8581629e879ac226ea584eba805b5664a1
YEJIA-MING <zstusbasa@gmail.com>
2024-10-24 21:42:37 +0800

    first tampermonkey

--- Dcard url.user.js
+++ Dcard url.user.js
@@ -6,7 +6,7 @@
 // @license      MIT
 // @author       You
 // @match        https://www.dcard.tw/f/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=dcard.tw
+// @icon         https://www.dcard.tw/favicon.ico
 // @grant        none
 // ==/UserScript==
 
