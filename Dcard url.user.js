08b2f04386cc984deaa6dbbc95cd73c4c5e9f861
YEJIA-MING <zstusbasa@gmail.com>
2024-10-24 23:14:32 +0800

    first tampermonkey

--- Dcard url.user.js
+++ Dcard url.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dcard url
 // @namespace    http://tampermonkey.net/
-// @version      2024-10-24 5.0
+// @version      2024-10-24 6.0
 // @description  自動搜尋網站內所有包含 http 的鏈接
 // @author       You
 // @match        https://www.dcard.tw/f/*
